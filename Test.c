3a20dcd5f43c2e3470a367770a0adffc469eb73f
Rem_Lovr <113937604@qq.com>
2026-05-25 21:37:44 +0800

    新增排序算法：归并排序、随机快排、随机选择算法
    - 实现归并排序（Merge函数）
    - 实现经典 partition 和荷兰国旗优化版 partition
    - 实现随机快排（quickSort）
    - 实现随机选择算法（randomizedSelect）

--- Test.c
+++ Test.c
@@ -1,7 +1,36 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
+#define MAX 501
+//归并过程,把两个有序的子数组合并成一个有序的大数组
+int arr[MAX];
+int help[MAX];
+int n;
+void Merge(int l,int m,int r)
+{
+	int i = l;
+	int j = m + 1;
+	int k = l;
+	while (i <= m && j <= r)
+	{
+		if (arr[i] <= arr[j])
+		{
+			help[k++] = arr[i++];
+		}
+		else
+		{
+			help[k++] = arr[j++];
+		}
+	}
+	//有一个已经为空了
+	while(i<=m) help[k++] = arr[i++];
+	while (j <= r) help[k++] = arr[j++];
+	for (int p = l; p <= r; p++)
+	{
+		arr[p] = help[p];
+		
+	}
+}
 //选择排序
 void SelectionSort(int* arr, int length)
 {
@@ -178,6 +207,134 @@ int FindPeak(int* arr, int length)
 	//没找到(基本不存在)
 	return -1;
 }
+//归并排序 递归版
+void MergeSort1(int l, int r)
+{
+	if (l == r)
+	{
+		return;
+	}
+	int m = (l + r) / 2;
+	MergeSort(l, m);
+	MergeSort(m + 1, r);
+	Merge(l, m, r);
+}
+//归并排序，非递归
+void MergeSort2()
+{
+	for (int step = 1; step < n; step <<= 1)
+	{
+		int l = 0;
+		int m;
+		int r;
+		while (l < n)
+		{
+			m = l + step - 1;
+			if (m + 1 >= n)//右边为空
+			{
+				break;
+			}
+			//右边不为空
+			//分为右边完全和不完全两种情况
+			if (m + 1 + step - 1 >= n)
+			{
+				r = n - 1;
+			}
+			else//右边完全
+			{
+				r = m + 1 + step - 1;
+			}
+			Merge(l, m, r);
+			l = r + 1;//移动到下一组
+		}
+	}
+}
+// 交换数组元素
+void swap(int i, int j) {
+	int tmp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = tmp;
+}
+//经典 partition
+int  partition1(int l, int r, int x)
+{
+	int a = l;
+	int xi;
+	for (int i = l; i <= r; i++)
+	{
+		if (arr[i]<x)
+		{
+			swap(a, i);
+			if (arr[a] == arr[i])
+			{
+				xi = i;
+			}
+			a++;
+		}
+	}
+	//i走到r的位置了，走到尽头了
+	swap(xi, a - 1);
+	return a - 1;
+}
+//优化partition,荷兰国旗问题
+int first;
+int last;
+void partition(int l, int r, int x)
+{
+	first = l;
+	last = r;
+	for (int i = l; i <= last; i++)
+	{
+		if ( arr[i]<x)
+		{
+			swap(first, i);
+			first++;
+		}
+		else if (arr[i] > x)
+		{
+			swap(last, i);
+			last--;
+			i--;
+		}
+
+	}
+}
+//随机快排
+void quickSort(int l, int r)
+{
+	if (l == r)
+	{
+		return;
+	}
+	int x = arr[l + rand() % (r - l + 1)];
+	int mid = partition1(l, r, x);
+	quickSort(l, mid - 1);
+	quickSort(mid + 1, r);
+}
+//随机选择算法，返回下标i的元素
+int randomizedSelect(int l, int r, int i)
+{
+	int ans = 0;
+	while (l <= r)
+	{
+		int randIdx = l + rand() % (r - l + 1);
+		int point = arr[randIdx];
+		partition(l, r, point);
+		if (i<first)
+		{
+			r = first - 1;
+		}
+		else if (i > last)
+		{
+			l = last + 1;
+		}
+		else //命中
+		{
+			ans = arr[i]; break;
+		}
+	}
+	return ans;
+}
 int main()
 {
 	int arr[] = { 1,5,4,3,2 };
@@ -186,3 +343,5 @@ int main()
 	SortPrint(arr, length);
 	return 0;
 }
+
+
