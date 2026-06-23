ff8fdbe948b6880892389c8a733f034e02396547
Rem_Lovr <113937604@qq.com>
2026-06-23 12:23:28 +0800

    feat: 新增C语言题库test21-test48练习题
    包含水仙花数、排序、字符串合并、阶乘、最大公约数等基础编程题。

--- c语言题库.c
+++ c语言题库.c
@@ -267,3 +267,405 @@ void test20()
 	int m = 560 % 60;
 	printf("%d小时 %d分钟", h, m);
 }
+//21.三位数求各位数字之和
+void test21()
+{
+	int m;
+	scanf("%d", &m);
+	int a = m / 100;
+	int b = m / 10 % 10;
+	int c = m % 10;
+	int sum = a + b + c;
+	printf("%d\n", sum);
+}
+//22
+void test22()
+{
+	float sum = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		sum += (1.0 / i);
+	}
+	printf("%.4f\n", sum);
+}
+//23水仙花
+void test23()
+{
+	for (int i = 100; i <= 999; i++)
+	{
+		int a = i / 100;//百位
+		int b = i / 10 % 10;//十位
+		int c = i % 10;//个位
+		int sum = a * a * a + b * b * b + c * c * c;
+		if (sum == i)
+		{
+			printf("%d\n", i);
+		}
+	}
+}
+//24小写字母转大写字母
+void test24()
+{
+	char m;
+	scanf("%c", &m);
+	m -= 32;
+	printf("%c\n", m);
+}
+//25排序
+void test25()
+{
+	int arr[4];
+	for (int i = 0; i < 4; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	//冒泡              n-1
+	for (int i = 0; i < 4-1; i++)
+	{
+		for (int j = 0; j < 4-1-i; j++)
+		{
+			if (arr[j] > arr[j+1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+	//输出
+	for (int m = 0; m < 4; m++)
+	{
+		printf("%d ", arr[m]);
+	}
+	printf("\n");
+}
+//26.求圆周率前五位
+void test26()
+{
+	float a = 355.0;
+	float b = 113.0;
+	printf("%.5f\n", a / b);
+}
+//27.输入x输出y
+void test27()
+{
+	int x;
+	int y;
+	scanf("%d", &x);
+	if (x < 0)
+	{
+		y = -1;
+	}
+	else if (x == 0)
+	{
+		y = 0;
+	}
+	else
+	{
+		y = 1;
+	}
+	printf("%d\n", y);
+}
+//28.求最小公倍数
+void test28()
+{
+	int a;
+	int b;
+	scanf("%d %d", &a, &b);
+	int m = a;
+	int n = b;
+	int temp;
+	while (b)
+	{
+		temp = a % b;
+		a = b;
+		b = temp;
+	}
+	int c = m * n / a;
+	printf("%d\n", c);
+}
+//29.输入十个数，求最大值
+void test29()
+{
+	int arr[10];
+	for (int i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	//冒泡
+	for (int i = 0; i < 10-1; i++)
+	{
+		for (int j = 0; j < 10 - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	printf("%d\n", arr[9]);
+}
+//30输入英文字母，输出ASCII编码值
+void test30()
+{
+	char a;
+	scanf("%c", &a);
+	printf("%d\n", a);
+}
+//31.输入票数，输出金额
+void test31()
+{
+	int a = 25;
+	int b = 20;
+	int m;
+	int sum;
+	scanf("%d", &m);
+	if (m < 30)
+	{
+		 sum = a * m;
+	}
+	else
+	{
+		sum = b * m;
+	}
+	printf("%d", sum);
+}
+//32
+void test32()
+{
+	for (int i = 200; i <= 300; i++)
+	{
+		int a = i / 100;
+		int b = i / 10 % 10;
+		int c = i % 10;
+		if (a * b * c == 36)
+		{
+			printf("%d", i);
+		}
+	}
+}
+//33.输入字符串，统计个数
+void test33()
+{
+	char arr[1000];
+	scanf("%s", arr);
+	int sum = 0;
+	while (arr[sum] != '\0')
+	{
+		sum++;
+	}
+	printf("%d\n", sum);
+}
+//34计算两个数和差积商
+void test34()
+{
+	float m, n;
+	scanf("%f %f", &m, &n);
+	float a = m + n;
+	float b = m - n;
+	float c = m * n;
+	float d = m / n;
+	printf("%.2f %.2f %.2f %.2f\n", a, b, c, d);
+}
+//35输入一个五位数，倒序输出各个位数字
+void test35()
+{
+	int m;
+	scanf("%d", &m);
+	int a = m / 10000;//万
+	int b = m / 1000 % 10;//千
+	int c = m / 100 % 10;//百12345
+	int d = m / 10%10;//十12345
+	int f = m % 10;//个
+	printf("%d\n", f);
+	printf("%d\n", d);
+	printf("%d\n", c);
+	printf("%d\n", b);
+	printf("%d\n", a);
+}
+//36.求n的阶乘
+void test36()
+{
+	int n;
+	long long sum = 1;
+	scanf("%d", &n);
+	while (n != 1)
+	{
+		sum *= n;
+		n--;
+	}
+	printf("%lld\n", sum);
+}
+//37
+void test37()
+{
+	int arr[5];
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	int sum = 0;
+	for (int j = 0; j < 5; j++)
+	{
+		if (j % 2 == 0)
+		{
+			sum += arr[j];
+		}
+	}
+	printf("%d\n", sum);
+}
+//38.输入两个数求平方和
+void test38()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	int sum = a * a + b * b;
+	printf("%d\n", sum);
+}
+//39.分段函数，输入x输出y
+void test39()
+{
+	int x,y;
+	scanf("%d", &x);
+	if (x > 0)
+	{
+		y = 18;
+	}
+	else if (x == 0)
+	{
+		y = 0;
+	}
+	else
+	{
+		y = -10;
+	}
+	printf("%d\n", y);
+}
+//40.输入三个数，找最大值
+void test40()
+{
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	int max = a;
+	max = (max > b) ? max : b;
+	max = (max > c) ? max : c;
+	printf("%d\n", max);
+}
+//41.输入10个数，逆序打印
+void test41()
+{
+	int arr[10];
+	for (int i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	for (int j = 9; j >= 0; j--)
+	{
+		printf("%d ", arr[j]);
+	}
+	printf("\n");
+}
+//42.转换摄氏度
+void test42()
+{
+	int f;
+	scanf("%d", &f);
+	float c = 5.0 / 9 * (f - 32);
+	printf("%.2f", c);
+}
+//43.输入三个数从小到大输出
+void test43()
+{
+	int x, y, z;
+	scanf("%d %d %d", &x, &y, &z);
+	int a, b, c;
+	//a最大
+	a = (x > y) ? x : y;
+	a = (a > z) ? a : z;
+	//c最小
+	c = (x < y) ? x : y;
+	c = (c < z) ? c : z;
+	//b中
+	b = x + y + z - a - c;
+	printf("%d %d %d\n", a, b, c);
+}
+//44.求600-800奇数和
+void test44()
+{
+	int sum = 0;
+	for (int i = 600; i <= 800; i++)
+	{
+		if (i % 2 != 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
+//45.输入10个数，去掉最大的，求其余九个数的平均数
+void test45()
+{
+	int arr[10];
+	for (int i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 0; i < 10 - 1; i++)
+	{
+		for (int j = 0; j < 10 - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	float sum = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		sum += arr[i];
+	}
+	printf("%.2f\n", sum / 9);
+}
+//46.输入三个数，计算和和平均数
+void test46()
+{
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	int sum = a + b + c;
+	float p = sum / 3.0;
+	printf("%d %.2f", sum, p);
+}
+//47.求一天有多少分钟
+void test47()
+{
+	int sum = 24 * 60 ;
+	printf("%d\n", sum);
+}
+//48.合并字符串
+void test48()
+{
+	char arr[100];
+	char brr[100];
+	scanf("%s %s", arr, brr);
+	int i = 0;
+	while (arr[i] != '\0')
+	{
+		i++;
+	}
+	int j = 0;
+	while (brr[j] != '\0')
+	{
+		arr[i] = brr[j];
+		i++;
+		j++;
+	}
+	arr[i] = '\0';
+	printf("%s\n", arr);
+}
+//49
+//50.
